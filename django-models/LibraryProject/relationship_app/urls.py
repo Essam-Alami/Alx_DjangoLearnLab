a1d1291a17edbea69ba5f90e3c9856d9996d3d13
VampyrFangs <testezmonclic@gmail.com>
2025-09-07 20:53:09 +0100

    ......

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,19 +1,15 @@
-from django.urls import path, include
-from .views import list_books, LibraryDetailView
-from .views import CustomLoginView, CustomLogoutView, register
-from django.contrib import admin
+from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from . import views
+from .views import list_books, LibraryDetailView, register, admin_view, librarian_view, member_view
 
 urlpatterns = [
+    path('', list_books, name='home'),
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', views.register, name='register'),
+    path('register/', register, name='register'),
+    path('admin-view/', admin_view, name='admin_view'),
+    path('librarian-view/', librarian_view, name='librarian_view'),
+    path('member-view/', member_view, name='member_view'),
 ]
