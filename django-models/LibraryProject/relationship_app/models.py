3990b8b4ad3dc10196e64f33388be01aede90bed
VampyrFangs <testezmonclic@gmail.com>
2025-09-07 19:26:32 +0100

    ........

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
