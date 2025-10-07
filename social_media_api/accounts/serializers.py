32e6da43ddeea5e214db24e220f918b7a6400e96
VampyrFangs <testezmonclic@gmail.com>
2025-10-07 20:52:17 +0100

    Fixed a syntax error

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -7,11 +7,10 @@ User = get_user_model()
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     password2 = serializers.CharField(write_only=True)
-    token = serializers.CharField(read_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2', 'token']
+        fields = ['username', 'email', 'password', 'password2']
 
     def validate(self, data):
         if data['password'] != data['password2']:
@@ -22,8 +21,8 @@ class RegisterSerializer(serializers.ModelSerializer):
         password = validated_data.pop('password')
         validated_data.pop('password2', None)
         user = get_user_model().objects.create_user(**validated_data, password=password)
-        token = Token.objects.create(user=user)
-        user.token = token.key
+        Token.objects.create(user=user)
         return user
+
 
 
