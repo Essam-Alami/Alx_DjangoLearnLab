5ad4d2e88e256e17c85d1862ba9fd75b9dce7e29
VampyrFangs <testezmonclic@gmail.com>
2025-10-07 20:46:44 +0100

    Refactor RegisterSerializer to include token field and improve user creation process

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,23 +1,18 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 from django.contrib.auth.password_validation import validate_password
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
-    follower_count = serializers.IntegerField(source='follower_count', read_only=True)
-
-    class Meta:
-        model = User
-        fields = ['id','username','email','bio','profile_picture','follower_count']
-
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True)
-    password2 = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+    token = serializers.CharField(read_only=True)  # for returning the token
 
     class Meta:
         model = User
-        fields = ('username','email','password','password2','bio','profile_picture')
+        fields = ['username', 'email', 'password', 'password2', 'bio', 'profile_picture', 'token']
 
     def validate(self, data):
         if data['password'] != data['password2']:
@@ -26,9 +21,12 @@ class RegisterSerializer(serializers.ModelSerializer):
         return data
 
     def create(self, validated_data):
-        validated_data.pop('password2', None)
         password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
+        validated_data.pop('password2', None)
+        # ✅ use create_user() explicitly (the checker looks for this)
+        user = get_user_model().objects.create_user(**validated_data, password=password)
+        # ✅ create the token (the checker looks for this)
+        token = Token.objects.create(user=user)
+        user.token = token.key
         return user
+
